4b9fba115a7de86c5893cfbe1a94f1d177e86de4
RelinDmitrii <relin-dmitrii@yandex.ru>
2021-04-11 09:34:19 +0300

    Hw

--- app/src/main/java/com/example/kotlinserver/MainActivity.kt
+++ app/src/main/java/com/example/kotlinserver/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.kotlinserver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.kotlinserver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initServiceWithBroadcastButton()
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(testReceiver, IntentFilter(TEST_BROADCAST_INTENT_FILTER))
+    }
+
+    private val testReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            //Достаем данные из Интента
+            intent.getStringExtra(THREADS_FRAGMENT_BROADCAST_EXTRA)?.let {
+               binding.textView.text = it
+            }
+        }
+    }
+
+
+    private fun initServiceWithBroadcastButton() {
+        binding.textView.setOnClickListener {
+            this?.let {
+                it.startService(Intent(it, MainService::class.java).apply {
+                    putExtra(
+                            MAIN_SERVICE_INT_EXTRA,
+                            binding.editText.text.toString().toInt()
+                    )
+                })
+            }
+        }
     }
 }
